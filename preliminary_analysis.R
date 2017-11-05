71e2d43255dbce331d1f509f421bb4c6054a43ba
misaugstad <michaelssaugstad@gmail.com>
2017-11-05 13:03:11 -0500

    changed variable name, uncommented plot

--- preliminary_analysis.R
+++ preliminary_analysis.R
@@ -1,10 +1,11 @@
 library(tidyverse)
 library(dplyr)
 
-dat <- read.csv("data/merged_crash_data-11-4-17.csv", header = TRUE, sep = ",")
-dat[["incidence"]][is.na(dat[["incidence"]])] <- 0
-fit <- glm(incidence ~ total_population + housing_units + house_income, data = dat, family = gaussian())
+crash.data <- read.csv("data/merged_crash_data-11-4-17.csv", header = TRUE, sep = ",")
+crash.data[["incidence"]][is.na(crash.data[["incidence"]])] <- 0
+fit <- glm(incidence ~ total_population + housing_units + house_income, data = crash.data, family = gaussian())
 summary(fit)
 
-# ggplot(data, aes(x = total_population, y = incidence)) +
-#   stat_summary(fun.y = "mean", geom = "bar")
+ggplot(crash.data, aes(x = total_population, y = incidence)) +
+  stat_summary(fun.y = "mean", geom = "bar") +
+  theme_bw()
