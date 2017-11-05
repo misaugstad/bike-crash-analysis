d42a8b378ec927bca5c134171f1faba392c25e5c
misaugstad <michaelssaugstad@gmail.com>
2017-11-05 13:07:55 -0500

    changed incidence column to be called accidents

--- preliminary_analysis.R
+++ preliminary_analysis.R
@@ -3,9 +3,10 @@ library(dplyr)
 
 crash.data <- read.csv("data/merged_crash_data-11-4-17.csv", header = TRUE, sep = ",")
 crash.data[["incidence"]][is.na(crash.data[["incidence"]])] <- 0
-fit <- glm(incidence ~ total_population + housing_units + house_income, data = crash.data, family = gaussian())
+crash.data <- crash.data %>% mutate(accidents = incidence)
+fit <- glm(accidents ~ total_population + housing_units + house_income, data = crash.data, family = gaussian())
 summary(fit)
 
-ggplot(crash.data, aes(x = total_population, y = incidence)) +
+ggplot(crash.data, aes(x = total_population, y = accidents)) +
   stat_summary(fun.y = "mean", geom = "bar") +
   theme_bw()
