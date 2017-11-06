63415261954fe3bf3525285483e4123bb21705b3
misaugstad <michaelssaugstad@gmail.com>
2017-11-06 10:07:01 -0500

    updated dataset to include bike parking, added data generation script

--- preliminary_analysis.R
+++ preliminary_analysis.R
@@ -1,10 +1,15 @@
 library(tidyverse)
 library(dplyr)
 
-crash.data <- read.csv("data/merged_crash_data-11-4-17.csv", header = TRUE, sep = ",")
+crash.data <- read.csv("data/merged_crash_data.csv", header = TRUE, sep = ",")
 crash.data[["incidence"]][is.na(crash.data[["incidence"]])] <- 0
-crash.data <- crash.data %>% mutate(accidents = incidence)
-fit <- glm(accidents ~ total_population + housing_units + house_income, data = crash.data, family = gaussian())
+crash.data <- crash.data %>% rename(total_population = B00001e1,
+                                    housing_units = B00002e1,
+                                    house_income = B19001e1,
+                                    accidents = incidence)
+fit <- glm(accidents ~ total_population + housing_units + house_income,
+           data = crash.data,
+           family = gaussian())
 summary(fit)
 
 ggplot(crash.data, aes(x = total_population, y = accidents)) +
