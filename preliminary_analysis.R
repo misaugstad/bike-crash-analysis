7585f9b95b92df18bd8e74c833412ef422eb5e9d
Yuhan Luo <Yuhan@wireless-10-104-111-252.umd.edu>
2017-11-28 13:41:26 -0500

    update SVM
    Remove unnecessary columns

---
+++ preliminary_analysis.R
@@ -0,0 +1,61 @@
+library(tidyverse)
+library(dplyr)
+library(caret)
+library(e1071)
+
+crash.data<- read.csv(file="~/Dropbox/Course@UMD/INFM750/Project/R scripts/All_data.csv", header=TRUE, sep=",")
+#crash.data[["incidence"]][is.na(crash.data[["incidence"]])] <- 0
+#crash.data$accidents <- rowSums(crash.data[11:118])
+crash.data<- na.omit(crash.data)
+crash.data <- transform(crash.data,
+                        accidents = Accidents_12_B15 + Accidents_13_B15 + Accidents_14_B15 + Accidents_15_B15 + Accidents_16_B15 + Accidents_17_B15)
+print(table(crash.data$accidents))
+
+
+#remove zero-accident intersections
+crash.data<-subset(crash.data, accidents!=0)
+
+# GLM
+glmfit<- glm(accidents ~ total_population + housing_units + household_income + NEAR_DIST + Width_max + Rating_min + Speed_max + ACC_max + OneWay_max,
+           data = crash.data,
+           family = gaussian())
+
+print(summary(glmfit))
+
+#drop unneccassry columns
+crash.data <- subset(crash.data, select = -c(11:118))
+write.csv(crash.data, file= "~/Dropbox/Course@UMD/INFM750/Project/R scripts/bikecrash_2012.csv")
+# required package for svm: install.packages('caret', dependencies = TRUE)
+
+# construct training and testing dataset
+set.seed(5)
+intrain <- createDataPartition(y = crash.data$accidents, p= 0.8, list = FALSE)
+training <- crash.data[intrain,]
+testing <- crash.data[-intrain,]
+print(dim(training) + dim(testing))
+
+
+# I'm not sure if we run it as a regression, how to measure the predication performance, so I coverted accidents into categorical in order to run classification.
+training[["accidents"]] = factor(training[["accidents"]])
+testing[["accidents"]] = factor(testing[["accidents"]])
+
+trctrl <- trainControl(method = "repeatedcv", number = 5, repeats = 3)
+set.seed(5)
+
+# SVM
+svmfit <- train(accidents ~., data = training, method = "svmLinear",
+                    trControl=trctrl,
+                    preProcess = c("center", "scale"),
+                    tuneLength = 5,
+                    probability = TRUE)
+print(summary(svmfit))
+
+svmfit <- svm(accidents ~., data = training )
+
+test_pred <- predict(svmfit, testing)
+
+plot(test_pred)
+
+#testing <- transform(testing,pred_acc = test_pred)
+print(confusionMatrix(data = test_pred, reference = testing$accidents))
+print(confusionMatrix(data = test_pred, reference = testing$accidents, mode = "prec_recall"))
