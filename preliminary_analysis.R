a2a210a5bd839e5ce61ae6c7d10ffd5e0350b768
Yuhan Luo <Yuhan@wireless-10-104-111-252.umd.edu>
2017-11-28 17:02:32 -0500

    sampled the data for classification

--- preliminary_analysis.R
+++ preliminary_analysis.R
@@ -11,37 +11,56 @@ crash.data <- transform(crash.data,
                         accidents = Accidents_12_B15 + Accidents_13_B15 + Accidents_14_B15 + Accidents_15_B15 + Accidents_16_B15 + Accidents_17_B15)
 print(table(crash.data$accidents))
 
+#drop unneccassry columns
+crash.data <- subset(crash.data, select = -c(13:120))
+crash.data <- subset(crash.data, select = -c(1))
+
 # print the percentage of zero-accident intersections
 print(nrow(crash.data[crash.data$accidents %in% 0,])/nrow(crash.data))
 
+# classify zero- and non-zero-accidents interactions
+crash.data$accidents_class <- cut(crash.data$accidents, c(-Inf, 0, Inf),
+                            labels=c('0', '>0'))
+print(table(crash.data$accidents_class))
+
+# sampling
+set.seed(9560)
+# crash.data <- downSample(x = crash.data[, -ncol(crash.data)], y = crash.data$accidents_class)
+# crash.data <- as.data.frame(crash.data)
+# colnames(crash.data)  <-c("Zero","Nonzero")
+crash.data <- downSample(x = crash.data[, -ncol(crash.data)],
+                         y = crash.data$accidents_class)
+table(crash.data$Class)
+
+# drop accidents
+crash.data <- subset(crash.data, select = -c(accidents))
+
 # remove zero-accident intersections
-crash.data<-subset(crash.data, accidents!=0)
+#crash.data<-subset(crash.data, accidents!=0)
 
 # GLM
-glmfit<- glm(accidents ~ total_population + housing_units + household_income + NEAR_DIST + Width_max + Rating_min + Speed_max + ACC_max + OneWay_max,
+glmfit<- glm(Class ~ total_population + housing_units + household_income + NEAR_DIST + Width_max + Rating_min + Speed_max + ACC_max + OneWay_max,
              data = crash.data,
              family = gaussian())
 
 print(summary(glmfit))
 
 
-#drop unneccassry columns
-crash.data <- subset(crash.data, select = -c(11:120))
-crash.data <- subset(crash.data, select = -c(1))
-
 #crash.data[["accidents"]] = factor(crash.data[["accidents"]])
 
 #crash.data$accidents<-cut(crash.data$accidents, c(0,1,2,4,5,10))
 
-crash.data$accidents <- cut(crash.data$accidents, c(-Inf, 1, Inf),
-                    labels=c('1', '>1'))
-print(table(crash.data$accidents))
+# segment the data
+# crash.data$accidents <- cut(crash.data$accidents, c(-Inf, 1, Inf),
+#                     labels=c('1', '>1'))
+# print(table(crash.data$accidents))
+
 #write.csv(crash.data, file= "bikecrash.csv")
 # required package for svm: install.packages('caret', dependencies = TRUE)
 
 # construct training and testing dataset
-set.seed(5)
-intrain <- createDataPartition(y = crash.data$accidents, p= 0.8, list = FALSE)
+set.seed(9560)
+intrain <- createDataPartition(y = crash.data$Class, p= 0.8, list = FALSE)
 training <- crash.data[intrain,]
 testing <- crash.data[-intrain,]
 print(dim(training) + dim(testing))
@@ -52,10 +71,10 @@ print(dim(training) + dim(testing))
 # testing[["accidents"]] = factor(testing[["accidents"]])
 
 trctrl <- trainControl(method = "repeatedcv", number = 5, repeats = 3)
-set.seed(5)
+set.seed(9560)
 
 # SVM
-svmfit <- train(accidents ~., data = training, method = "svmLinear",
+svmfit <- train(Class ~., data = training, method = "svmLinear",
                 trControl=trctrl,
                 preProcess = c("center", "scale"),
                 tuneLength = 5,
@@ -69,5 +88,5 @@ test_pred <- predict(svmfit, testing)
 plot(test_pred)
 
 #testing <- transform(testing,pred_acc = test_pred)
-print(confusionMatrix(data = test_pred, reference = testing$accidents))
-print(confusionMatrix(data = test_pred, reference = testing$accidents, mode = "prec_recall"))
+print(confusionMatrix(data = test_pred, reference = testing$Class))
+print(confusionMatrix(data = test_pred, reference = testing$Class, mode = "prec_recall"))
