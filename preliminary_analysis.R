01bc885e4d7b785b63d91b33f4fd09d120ad2665
Yuhan Luo <Yuhan@wireless-10-104-111-252.umd.edu>
2017-11-28 15:18:39 -0500

    updated analysis file with data

--- preliminary_analysis.R
+++ preliminary_analysis.R
@@ -3,17 +3,19 @@ library(dplyr)
 library(caret)
 library(e1071)
 
-crash.data<- read.csv(file="~/Dropbox/Course@UMD/INFM750/Project/R scripts/All_data.csv", header=TRUE, sep=",")
+crash.data<- read.csv(file="All_data.csv", header=TRUE, sep=",")
 #crash.data[["incidence"]][is.na(crash.data[["incidence"]])] <- 0
-#crash.data$accidents <- rowSums(crash.data[11:118])
+#crash.data$accidents <- rowSums(crash.data[11:120])
 crash.data<- na.omit(crash.data)
 crash.data <- transform(crash.data,
                         accidents = Accidents_12_B15 + Accidents_13_B15 + Accidents_14_B15 + Accidents_15_B15 + Accidents_16_B15 + Accidents_17_B15)
 print(table(crash.data$accidents))
 
+# print the percentage of zero-accident intersections
+print(nrow(crash.data[crash.data$accidents %in% 0,])/nrow(crash.data))
 
 #remove zero-accident intersections
-crash.data<-subset(crash.data, accidents!=0)
+#crash.data<-subset(crash.data, accidents!=0)
 
 # GLM
 glmfit<- glm(accidents ~ total_population + housing_units + household_income + NEAR_DIST + Width_max + Rating_min + Speed_max + ACC_max + OneWay_max,
@@ -22,9 +24,19 @@ glmfit<- glm(accidents ~ total_population + housing_units + household_income + N
 
 print(summary(glmfit))
 
+
 #drop unneccassry columns
-crash.data <- subset(crash.data, select = -c(11:118))
-write.csv(crash.data, file= "~/Dropbox/Course@UMD/INFM750/Project/R scripts/bikecrash_2012.csv")
+crash.data <- subset(crash.data, select = -c(11:120))
+crash.data <- subset(crash.data, select = -c(1))
+
+crash.data[["accidents"]] = factor(crash.data[["accidents"]])
+
+#crash.data$accidents<-cut(crash.data$accidents, c(0,1,2,4,5,10))
+
+# crash.data$accidents <- cut(crash.data$accidents, c(-Inf, 0, 1, 3, 6, 10, Inf),
+#                    labels=c('<1', '1', '2-3', '4-5','6-10', '>10'))
+
+#write.csv(crash.data, file= "bikecrash.csv")
 # required package for svm: install.packages('caret', dependencies = TRUE)
 
 # construct training and testing dataset
@@ -36,8 +48,8 @@ print(dim(training) + dim(testing))
 
 
 # I'm not sure if we run it as a regression, how to measure the predication performance, so I coverted accidents into categorical in order to run classification.
-training[["accidents"]] = factor(training[["accidents"]])
-testing[["accidents"]] = factor(testing[["accidents"]])
+# training[["accidents"]] = factor(training[["accidents"]])
+# testing[["accidents"]] = factor(testing[["accidents"]])
 
 trctrl <- trainControl(method = "repeatedcv", number = 5, repeats = 3)
 set.seed(5)
@@ -50,7 +62,7 @@ svmfit <- train(accidents ~., data = training, method = "svmLinear",
                     probability = TRUE)
 print(summary(svmfit))
 
-svmfit <- svm(accidents ~., data = training )
+svmfit <- svm(accidents ~., data = training)
 
 test_pred <- predict(svmfit, testing)
 
